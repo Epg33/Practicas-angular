6c97f56c3a9e0cd6fa701eeafff5aa41b6886994
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-10-11 11:31:26 -0500

    calculadora simple

--- typecript-intro/index.js
+++ typecript-intro/index.js
@@ -2,6 +2,20 @@
 console.log('hola');
 let grupo = 10;
 const resolve = (a, b, operation) => {
-    return 0;
+    switch (operation) {
+        case 'add':
+            return a + b;
+        case 'divide':
+            if (b === 0) {
+                return 'no se puede dividir entre cero pana';
+            }
+            return a / b;
+        case 'multiplie':
+            return a * b;
+        case 'subtract':
+            return a - b;
+        default:
+            return 'operacion invalida';
+    }
 };
-resolve(15, 20, 'add');
+console.log(resolve(15, 20, 'add'));
--- typecript-intro/index.ts
+++ typecript-intro/index.ts
@@ -1,12 +1,26 @@
 console.log('hola');
 
-let grupo: number = 10;
+let grupo:number = 10;
 
 type operations = 'subtract'|'add'|'divide'|'multiplie'
+type result = number|string
 
-const resolve = (a:number, b:number, operation:operations):number => {
- 
-  return 0;
+const resolve = (a:number, b:number, operation:operations):result => {
+  switch(operation){
+    case 'add':
+      return a+b;
+    case 'divide':
+      if(b===0){
+        return 'no se puede dividir entre cero pana';
+      }
+      return a/b;
+    case 'multiplie':
+      return a*b
+    case 'subtract':
+      return a-b
+    default:
+      return 'operacion invalida'
+  }
 }
 
-resolve(15, 20, 'add');
+console.log(resolve(15, 20, 'add'));
