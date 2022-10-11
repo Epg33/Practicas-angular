f7343ecdd657ea5c02faf231c2d40eff1f797fda
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-10-11 11:18:23 -0500

    funcion de operacion simple

--- typecript-intro/index.js
+++ typecript-intro/index.js
@@ -1,3 +1,7 @@
 "use strict";
 console.log('hola');
 let grupo = 10;
+const resolve = (a, b, operation) => {
+    return 0;
+};
+resolve(15, 20, 'add');
--- typecript-intro/index.ts
+++ typecript-intro/index.ts
@@ -2,3 +2,11 @@ console.log('hola');
 
 let grupo: number = 10;
 
+type operations = 'subtract'|'add'|'divide'|'multiplie'
+
+const resolve = (a:number, b:number, operation:operations):number => {
+ 
+  return 0;
+}
+
+resolve(15, 20, 'add');
