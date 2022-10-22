465682ea81ed4ac2c7a50e31438767a719a18a13
EthiemGuerrero <ethiemalex03@gmail.com>
2022-10-22 17:14:22 -0500

    cambios varios

--- intro/src/app/app.module.ts
+++ intro/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
