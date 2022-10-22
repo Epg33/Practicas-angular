49b1f8752d706358c1cdeb45c7ae01b26a324a07
EthiemGuerrero <ethiemalex03@gmail.com>
2022-10-22 17:26:59 -0500

    continuar

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `<h1>Hello Wolrd from inline template</h1>
+  // <p>Angular is Awesome</p>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
