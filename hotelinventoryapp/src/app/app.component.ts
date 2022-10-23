496c34df9432eddb12b56b42549983c5cef37b78
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-10-23 14:20:41 -0500

    inicio de un poco de codigo

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   templateUrl: './app.component.html',
   // template: `<h1>Hello Wolrd from inline template</h1>
   // <p>Angular is Awesome</p>`,
