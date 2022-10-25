67024025b2b35e979f28167a501043d69b040b24
EthiemGuerrero <ethiemalex03@gmail.com>
2022-10-24 20:48:53 -0500

    componenetes e inicio de enrutado

--- cursos/src/app/app-routing.module.ts
+++ cursos/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './componente/login/login.component';
+import { RegistroComponent } from './componente/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'', component: LoginComponent},
+{path: 'registro', component: RegistroComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cursos/src/app/app.module.ts
+++ cursos/src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './componente/nav/nav.component';
+import { LoginComponent } from './componente/login/login.component';
+import { FooterComponent } from './componente/footer/footer.component';
+import { RegistroComponent } from './componente/registro/registro.component';
+import { MisionComponent } from './componente/mision/mision.component';
+import { CursosComponent } from './componente/cursos/cursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoginComponent,
+    FooterComponent,
+    RegistroComponent,
+    MisionComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
